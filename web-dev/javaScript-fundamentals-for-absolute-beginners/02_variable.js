27c61d6eeeebbb0903c9770da34b957c6f9f7629
rakibul-wdp <rakibul.wdp@gmail.com>
2024-09-24 04:37:29 +0600

    variable

---
+++ web-dev/javaScript-fundamentals-for-absolute-beginners/02_variable.js
@@ -0,0 +1,17 @@
+/*
+variable
+4/5 things need to declare a variable
+*/
+
+var age = 24;
+var weight = 60;
+var price = 33;
+var year = 1950;
+var balance = 1500;
+
+balance = 1000;
+
+var width;
+width = 50;
+
+console.log(balance);
