b6bb93ccb5498bed1d0861066f65a2164e76f866
rakibul-wdp <rakibul.wdp@gmail.com>
2024-09-25 05:07:52 +0600

    types

---
+++ web-dev/javaScript-fundamentals-for-absolute-beginners/03_types.js
@@ -0,0 +1,14 @@
+// Number
+var age = 24;
+var price = 33;
+var year = 1950;
+
+// String
+var name = "abul babul";
+var address = "Dhaka";
+var country = 'Bangladesh';
+
+// boolean
+var isRich = true;
+var passed = false;
+console.log(typeof passed);
