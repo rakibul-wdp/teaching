51e7785a1218346a29107c334319c04a19ce24ae
rakibul-wdp <rakibul.wdp@gmail.com>
2024-09-28 04:43:11 +0600

    naming convention

---
+++ web-dev/javaScript-fundamentals-for-absolute-beginners/04_naming.js
@@ -0,0 +1,36 @@
+// keyword variableName = value;
+var score = 10;
+
+// 1. avoid using keywords in variable name
+var true = 33;
+var var = 45;
+
+// 2. no space or gaps are allowed in variable names
+var is good = false;
+var isGood = false;
+
+// 3. no quote
+var "address" = "omok zila";
+
+// 4. cannot begin with a number, but can include it after that
+var balance = 1500;
+var balance2 = 1000;
+var 3balnce = 500;
+
+// 5. names are case-sensitive
+place
+Place
+PLASE
+plaCe
+
+// 6. how to write a long variable name
+var my current home address = "omok zila tomok thana";
+var mycurrenthomeaddress = "omok zila tomok thana";
+var my_current_home_address = "omok zila tomok thana"; // snake case
+var myCurrentHomeAddress = "omok zila tomok thana"; // camel case
+var MyCurrentHomeAddress = "omok zila tomok thana"; // Pascal case
+
+// 7. keyword are case-sensitive
+var name = "rahim";
+VAR name = "karim";
+Var name = "korim";
