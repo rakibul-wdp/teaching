75154a41b5a5fc549b7215a08d8243f00f0f8c57
rakibul-wdp <rakibul.wdp@gmail.com>
2024-09-29 05:03:54 +0600

    number

---
+++ web-dev/javaScript-fundamentals-for-absolute-beginners/05_number.js
@@ -0,0 +1,61 @@
+var applePrice = 25;
+var chocolatePrice = 3.5;
+// var mangoPrice = "15";
+// var mangoPrice = parseInt("15");
+// var mangoPrice = parseInt("adljadd");
+// var mangoPrice = parseInt("15.5");
+var mangoPrice = parseFloat("15.5");
+
+// console.log(typeof applePrice);
+// console.log(mangoPrice);
+
+// console.log(applePrice + mangoPrice);
+
+
+
+
+var num1 = 0.1;
+var num2 = 0.2;
+
+var total = num1 + num2;
+// console.log(total.toFixed(1));
+// console.log(parseFloat(total.toFixed(1)));
+
+
+// var num = 50 / 0;
+var num = -50 / 0;
+console.log(num);
+
+
+
+
+
+
+
+
+
+
+
+/*
+0.1 + 0.2 = 0.30000000000000004
+
+1/3 + 1/3 + 1/3 = 3/3 = 1
+
+1   1   1   3
+- + - + - = - = 1 
+3   3   3   3
+
+1/3 = 0.3333333333333 ∞
+
+0.3 ∞ + 0.3 ∞ + 0.3 ∞ = 1
+
+1/3 = 0.3333
+
+0.3333 + 0.3333 + 0.3333 = 0.9999
+
+
+0.1 - 0.00011001100110011001 ∞
+0.2 - 0.00110011001100110011 ∞
+-------------------------------
+0.3 - 0.30000000000000004
+*/
