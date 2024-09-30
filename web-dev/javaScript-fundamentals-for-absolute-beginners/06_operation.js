1791ca9e2491a4d76b8f65ce8516c0778dd65c24
rakibul-wdp <rakibul.wdp@gmail.com>
2024-10-01 04:42:02 +0600

    operation

---
+++ web-dev/javaScript-fundamentals-for-absolute-beginners/06_operation.js
@@ -0,0 +1,31 @@
+var balance = 100;
+var eggPrice = 10;
+var potatoPrice = 20;
+
+console.log(eggPrice + potatoPrice);
+console.log(balance - eggPrice);
+console.log(eggPrice * potatoPrice); // 10 X 20
+
+/*
+3 | 10 | 3
+     9
+  ------
+     1
+*/
+
+console.log(balance / eggPrice);
+console.log(10 % 3);
+console.log(11 % 5);
+
+var money = 100;
+money = 80;
+
+// ------------
+
+var shirtPrice = 500;
+shirtPrice = shirtPrice + 100;
+console.log(shirtPrice);
+
+var newPrice = shirtPrice + 150;
+var discountPrice = newPrice - 50;
+console.log(discountPrice);
