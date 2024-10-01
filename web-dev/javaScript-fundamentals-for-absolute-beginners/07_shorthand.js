ab20c3b2bed4261ef979f3d1e48fa211c3c30c94
rakibul-wdp <rakibul.wdp@gmail.com>
2024-10-02 04:45:58 +0600

    shorthand

---
+++ web-dev/javaScript-fundamentals-for-absolute-beginners/07_shorthand.js
@@ -0,0 +1,38 @@
+var price = 25;
+// price = price + 10;
+price += 10; // price = price + 10;
+// console.log(price);
+
+var balance = 100;
+// balance = balance - 50;
+balance -= 50;
+// console.log(balance);
+
+var applePrice = 20;
+// applePrice = applePrice * 3;
+applePrice *= 3;
+// console.log(applePrice);
+
+var stockQuantity = 20;
+// stockQuantity = stockQuantity / 2;
+stockQuantity /= 2;
+// console.log(stockQuantity);
+
+
+
+// concatenation
+
+var firstName = "abul";
+var lastName = "babul";
+
+var fullName = firstName + lastName;
+// console.log(fullName);
+
+// console.log(25 + "2");
+// console.log("25" + 2);
+
+// console.log(25 - "5");
+// console.log("30" - 10);
+
+// console.log(10 * "5");
+console.log("20" / 4);
