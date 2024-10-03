fd7e96416b6b757ceba51cb7ef66875cc1c03174
rakibul-wdp <rakibul.wdp@gmail.com>
2024-10-03 22:02:25 +0600

    comparison

---
+++ web-dev/javaScript-fundamentals-for-absolute-beginners/08_comparison.js
@@ -0,0 +1,43 @@
+/*
+comparison
+  - bigger/greater: >
+  - less: <
+  - equal: ==
+  - not equal: !=
+  - greater that or equal: >=
+  - less than or equal: <=
+
+  - logical and: &&
+  - logical or: ||
+*/
+
+// console.log(5 > 10);
+// console.log(5 < 10);
+// console.log(5 == 10);
+// console.log(10 == 10);
+// console.log(10 >= 10);
+// console.log(15 >= 10);
+// console.log(5 >= 10);
+// console.log(5 <= 10);
+// console.log(10 <= 10);
+// console.log(20 <= 10);
+
+var applePrice = 25;
+var orangePrice = 30;
+
+// console.log(applePrice > orangePrice);
+// console.log(applePrice < orangePrice);
+
+/*
+==
+===
+*/
+
+// console.log(10 == 10);
+// console.log(10 == "10");
+// console.log(10 === "10");
+// console.log(10 == "hello");
+
+console.log(10 != "10");
+console.log(10 !== "10");
+console.log(5 != 5);
