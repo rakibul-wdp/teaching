e2b4f5839185552abbdda2260dccc24817ecce6f
rakibul-wdp <rakibul.wdp@gmail.com>
2024-10-05 04:42:24 +0600

    if else

---
+++ web-dev/javaScript-fundamentals-for-absolute-beginners/09_if-else.js
@@ -0,0 +1,47 @@
+/*
+if it rains
+  I will eat khichuri
+
+if it rains
+  I will go out with an umbrella
+
+if you come
+  I will go
+
+Structure
+
+if (condition) {
+  executes code if the condition is true
+}
+*/
+
+if (5 < 10) {
+  // console.log("5 is less than 10");
+}
+
+if (15 < 10) {
+  console.log("15 is less than 10");
+}
+
+// var cakePrice = 500;
+var cakePrice = 250;
+
+if (cakePrice < 300) {
+  // console.log("I will buy this cake");
+}
+
+/*
+if (condition) {
+  executes code if the condition is true.
+} else {
+  executes code if the condition is false.
+}
+*/
+
+var age = 15;
+
+if (age >= 18) {
+  console.log("You are eligible to vote");
+} else {
+  console.log("You are not eligible to vote");
+}
