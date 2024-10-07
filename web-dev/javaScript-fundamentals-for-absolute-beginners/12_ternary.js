b228bf79939dc7bff8b82ce0c46fe9e642b3ed6b
rakibul-wdp <rakibul.wdp@gmail.com>
2024-10-08 05:06:03 +0600

    ternary

---
+++ web-dev/javaScript-fundamentals-for-absolute-beginners/12_ternary.js
@@ -0,0 +1,64 @@
+const temperature = 20;
+
+// if (temperature >= 25) {
+//   console.log("warm day");
+// } else {
+//   console.log("day cold");
+// }
+
+/*
+Ternary - Refers to something composed of three parts or elements.
+
+structure
+condition ? execute if true : execute if false
+*/
+
+// temperature >= 25 ? console.log("warm day") : console.log("cold day");
+
+let total = 200;
+const hasCoupon = false;
+
+if (!hasCoupon) { // hasCoupon === true
+  total = total - 50;
+} else {
+  total = total + 20;
+}
+
+// console.log(total);
+
+// total = hasCoupon === true ? total - 50 : total + 20;
+
+// console.log(total);
+
+
+const number = 0;
+
+// if (number > 0) {
+//   console.log("Positive number");
+// } else if (number < 0) {
+//   console.log("Negative number");
+// } else {
+//   console.log("Zero");
+// }
+
+// number > 0 ? console.log("Positive number") : number < 0 ? console.log("Negative number") : console.log("Zero");
+
+const purchaseAmount = 250;
+const isMember = true;
+let payAmount;
+
+// if (isMember) { // isMember === true
+//   if (purchaseAmount > 200) {
+//     payAmount = purchaseAmount - 20;
+//   } else {
+//     payAmount = purchaseAmount - 10;
+//   }
+// } else {
+//   payAmount = purchaseAmount - 5;
+// }
+
+// console.log(payAmount);
+
+payAmount = isMember === true ? (purchaseAmount > 200 ? purchaseAmount - 20 : purchaseAmount - 10) : purchaseAmount - 5;
+
+console.log(payAmount);
