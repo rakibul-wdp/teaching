db74e2956ddabc10fda4817d1a3d0df8ea3a2603
rakibul-wdp <rakibul.wdp@gmail.com>
2024-10-09 04:40:11 +0600

    array

---
+++ web-dev/javaScript-fundamentals-for-absolute-beginners/13_array.js
@@ -0,0 +1,15 @@
+const num1 = 2;
+const num2 = 4;
+const num3 = 6;
+const num4 = 8;
+
+// const evenNumber = 2468;
+
+// array - a collection of multiple items under a single variable name
+
+// (), {}, []
+const evenNumbers = [2, 4, 6, 8];
+const friends = ["abul", "babul", "cabul", "dabul", "ebul"];
+const mixedArray = ["rahim", 24, true, "Dhaka"];
+
+console.log(evenNumbers);
