9a5aa4180a2b4df67dae5ff8da4873121f897895
rakibul-wdp <rakibul.wdp@gmail.com>
2024-10-10 05:26:35 +0600

    array length index

---
+++ web-dev/javaScript-fundamentals-for-absolute-beginners/14_array-length-index.js
@@ -0,0 +1,22 @@
+// array size
+const numbers = [10, 20, 30, 40, 50, 22, 30, 12];
+// console.log(numbers.length);
+
+// index
+const evenNumbers = [2, 4, 6, 8, 10, 12];
+//       index       0  1  2  3   4   5
+
+// get element by index
+// console.log(evenNumbers);
+// console.log(evenNumbers[0]);
+// console.log(evenNumbers[3]);
+
+const fifth = evenNumbers[4];
+// console.log(fifth);
+
+// set and update element by index
+const oddNumbers = [3, 6, 7];
+oddNumbers[3] = 9;
+oddNumbers[1] = 5;
+
+console.log(oddNumbers);
