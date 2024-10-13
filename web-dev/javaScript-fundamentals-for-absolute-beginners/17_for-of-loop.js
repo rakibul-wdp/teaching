00ad81816fb6a9b02f3de0eb40f19f415bfa9b61
rakibul-wdp <rakibul.wdp@gmail.com>
2024-10-14 04:37:48 +0600

    for of loop

---
+++ web-dev/javaScript-fundamentals-for-absolute-beginners/17_for-of-loop.js
@@ -0,0 +1,19 @@
+const numbers = [3, 5, 7, 8, 9, 12];
+
+// console.log(numbers);
+
+for (const number of numbers) {
+  // console.log(number);
+}
+
+const months = ["Jan", "Feb", "Mar", "Apr", "May"];
+
+/*
+for () {
+
+}
+*/
+
+for (const month of months) {
+  console.log(month);
+}
