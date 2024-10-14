a04cfbffbb8e9b899c78710e6518d6316c772875
rakibul-wdp <rakibul.wdp@gmail.com>
2024-10-15 04:57:51 +0600

    while loop

---
+++ web-dev/javaScript-fundamentals-for-absolute-beginners/18_while-loop.js
@@ -0,0 +1,17 @@
+const number = 15;
+
+if (number < 10) {
+  // console.log("inside if block");
+}
+
+// console.log("outside if block");
+
+let num = 0; // loop variable
+
+while (num < 5) {
+  console.log("looping ->", num);
+
+  // num = num + 1;
+  // num += 1;
+  num++;
+}
