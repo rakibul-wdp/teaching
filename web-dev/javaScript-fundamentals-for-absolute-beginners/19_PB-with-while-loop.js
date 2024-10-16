550eb1feb3621b337c4aeb311eed547f810a45ff
rakibul-wdp <rakibul.wdp@gmail.com>
2024-10-16 19:24:23 +0600

    problem solving with while loop

---
+++ web-dev/javaScript-fundamentals-for-absolute-beginners/19_PB-with-while-loop.js
@@ -0,0 +1,64 @@
+// Display the numbers from 1 to 10
+/*
+let num = 1;
+while (num <= 10) {
+  console.log(num);
+
+  num++;
+}
+*/
+
+// Find the sum of the numbers from 1 to 10
+/*
+let num = 1;
+let sum = 0;
+while (num <= 10) {
+  console.log(num);
+
+  sum = sum + num;
+  console.log("sum: ", sum);
+
+  num++;
+}
+*/
+
+// Find the even and odd numbers from 1 to 10
+/*
+let num = 1;
+while (num <= 10) {
+  // console.log(num);
+
+  // even numbers
+  if (num % 2 === 0) {
+    console.log("even number ", num);
+  }
+
+  // odd numbers
+  if (num % 2 != 0) {
+    console.log("odd number ", num);
+  }
+
+  num++;
+}
+*/
+
+// Bonus
+// even numbers
+/*
+let num = 2;
+while (num <= 10) {
+  console.log(num);
+
+  num += 2;
+}
+*/
+
+// odd numbers
+/*
+let num = 1;
+while (num <= 10) {
+  console.log(num);
+
+  num += 2;
+}
+*/
