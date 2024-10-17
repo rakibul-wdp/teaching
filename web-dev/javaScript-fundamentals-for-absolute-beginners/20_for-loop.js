b11c7d58b302829bcad3a345c4e3846b002f1891
rakibul-wdp <rakibul.wdp@gmail.com>
2024-10-18 04:52:18 +0600

    for loop

---
+++ web-dev/javaScript-fundamentals-for-absolute-beginners/20_for-loop.js
@@ -0,0 +1,28 @@
+/*
+let num = 0; // loop variable
+while (num < 5) { // loop condition
+  console.log(num); // loop statement
+
+  num++; // loop variable change (increment/decrement)
+}
+*/
+
+/*
+for (variable declaration; loop condition; increment/decrement) {
+  code/statements
+}
+*/
+
+for (let num = 0; num < 5; num++) {
+  // console.log(num);
+}
+
+// even numbers
+for (let i = 0; i <= 10; i = i + 2) {
+  // console.log(i);
+}
+
+// odd numbers
+for (let i = 1; i <= 10; i += 2) {
+  console.log(i);
+}
