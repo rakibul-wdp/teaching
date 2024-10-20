c59e96939ce8d0fb333da22d3dfcd8f3f7e97c1e
rakibul-wdp <rakibul.wdp@gmail.com>
2024-10-21 05:13:51 +0600

    string

---
+++ web-dev/javaScript-fundamentals-for-absolute-beginners/23_string.js
@@ -0,0 +1,23 @@
+const country = "Bangladesh";
+const division = 'Rajshahi';
+const district = `Bogura`;
+// const thana = new String("Sherpur");
+
+// console.log(thana);
+
+const numbers = [3, 5, 7, 8, 9];
+// console.log(numbers.length);
+// console.log(numbers[2]);
+// console.log(numbers);
+numbers[1] = 10;
+// console.log(numbers);
+
+const capital = "Dhaka";
+// console.log(capital.length);
+// console.log(capital[1]);
+console.log(capital);
+capital[1] = "E";
+console.log(capital);
+
+// string - immutable (not changeable)
+// array - mutable (changeable)
