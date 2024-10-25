4c15570e92027c1ed94065e1f9915bf3e390084c
rakibul-wdp <rakibul.wdp@gmail.com>
2024-10-25 20:13:09 +0600

    object

---
+++ web-dev/javaScript-fundamentals-for-absolute-beginners/27_object.js
@@ -0,0 +1,31 @@
+// primitive - immutable
+const studentName = "Rahim";
+const studentClass = "Ten";
+const studentRoll = 33;
+const studentAge = 18;
+
+// non-primitive
+const studentSubjects = ["Math", "Physics", "English"];
+
+// object
+// non-primitive - mutable
+const student = {
+  name: "Rahim",
+  class: "Ten",
+  roll: 33,
+  age: 18,
+  subjects: ["Math", "Physics", "English"],
+  address: {
+    street: "123 Main St",
+    city: "Dhaka",
+    country: "Bangladesh",
+  }
+}
+
+console.log(student);
+
+const mobilePhone = {
+  brand: "Orang",
+  model: "oPhone 20",
+  storage: "123 GB"
+}
