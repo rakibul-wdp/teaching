e52c3ae8b356743c7fa0d90edecaf8139e42e4a5
rakibul-wdp <rakibul.wdp@gmail.com>
2024-10-30 04:54:10 +0600

    array traverse with for and while loop

---
+++ web-dev/javaScript-fundamentals-for-absolute-beginners/31_array-traverse-for-while-loop.js
@@ -0,0 +1,30 @@
+const colors = ["Red", "Green", "Blue", "Yellow", "Purple"];
+
+for (const color of colors) {
+  // console.log(color);
+}
+
+for (let i = 0; i < colors.length; i++) {
+  // console.log(i);
+  // console.log(colors[i]);
+}
+
+const numbers = [3, 5, 7, 8, 9, 12, 13, 15, 20];
+
+for (let i = 0; i < numbers.length; i++) {
+  // console.log(numbers[i]);
+}
+
+let i = 0;
+while (i < colors.length) {
+  // console.log(colors[i]);
+
+  i++;
+}
+
+let n = 0;
+while (n < numbers.length) {
+  console.log(numbers[n]);
+
+  n++;
+}
