02e2c91b4bdf45bb12b8325b8621d36741ac60d5
rakibul-wdp <rakibul.wdp@gmail.com>
2024-11-03 19:53:56 +0600

    function

---
+++ web-dev/javaScript-fundamentals-for-absolute-beginners/34_function.js
@@ -0,0 +1,23 @@
+/*
+function functionName () {
+
+}
+*/
+
+function sayHello() {
+  console.log("Hello");
+}
+
+// call the function (invokes it)
+// sayHello();
+
+function makeCoffee() {
+  console.log("Boil water");
+  console.log("Add coffee to cup");
+  console.log("Pour water and stir");
+}
+
+makeCoffee();
+console.log("something");
+console.log("something else")
+makeCoffee();
