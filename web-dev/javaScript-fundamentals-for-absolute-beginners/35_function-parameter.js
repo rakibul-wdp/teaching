3948499313a9ace2906610c3448b5946e67f1d61
rakibul-wdp <rakibul.wdp@gmail.com>
2024-11-05 04:50:58 +0600

    function parameter

---
+++ web-dev/javaScript-fundamentals-for-absolute-beginners/35_function-parameter.js
@@ -0,0 +1,29 @@
+// 2 --> 2 * 2 = 4
+// 4 --> 4 * 4 = 8
+// 9 --> 9 * 9 = 81
+
+function calculateSquare(number) {
+  // console.log(number);
+
+  const squaredNumber = number * number;
+  console.log(squaredNumber);
+}
+
+// calculateSquare(2);
+// calculateSquare(4);
+// calculateSquare(358);
+
+function sumTwoNumbers(firstNumber, secondNumber) {
+  const sum = firstNumber + secondNumber;
+  console.log(sum);
+}
+
+// sumTwoNumbers(3, 5);
+// sumTwoNumbers(7, 12);
+
+function sumAll(a, b, c, d, e) {
+  const total = a + b + c + d + e;
+  console.log(total);
+}
+
+sumAll(3, 4, 5, 6, 7);
