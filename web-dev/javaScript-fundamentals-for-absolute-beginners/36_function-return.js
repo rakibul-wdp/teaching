09199f60b15e1ddde6924112fcca93e4d73ff880
rakibul-wdp <rakibul.wdp@gmail.com>
2024-11-07 04:46:31 +0600

    function return

---
+++ web-dev/javaScript-fundamentals-for-absolute-beginners/36_function-return.js
@@ -0,0 +1,11 @@
+function twoTimes(number) {
+  const result = number * 2;
+
+  return result;
+}
+
+twoTimes(3);
+console.log(twoTimes(4));
+
+const doubledValue = twoTimes(5);
+console.log(doubledValue);
